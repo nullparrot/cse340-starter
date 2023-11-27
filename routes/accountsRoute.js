2881994dc3bb7de135bf4fab59a385ff7bbedae5
nullparrot <github@weist.dev>
2023-11-26 19:00:48 -0800

    Build and format register page

--- routes/accountsRoute.js
+++ routes/accountsRoute.js
@@ -7,4 +7,7 @@ const accountsController = require("../controllers/accountsController")
 //Route to build login page
 router.get("/login", accountsController.buildLogin)
 
+//Route to build registration page
+router.get("/register", accountsController.buildRegister)
+
 module.exports = router
